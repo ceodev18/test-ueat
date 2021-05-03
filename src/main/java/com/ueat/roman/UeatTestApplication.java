7e5968a65ba8a833c6a8ec51743cb64c9e7ee9f3
Italo <espinoza.c@pucp.edu.pe>
2021-05-02 21:19:08 -0500

    setup completed

--- src/main/java/com/ueat/roman/UeatTestApplication.java
+++ src/main/java/com/ueat/roman/UeatTestApplication.java
@@ -1,6 +1,4 @@
 package com.ueat.roman;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
